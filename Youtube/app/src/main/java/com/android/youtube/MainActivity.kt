5849d9b2c45f5b0a2db1039492d9b19d3e12ae6c
Fairooz <fairoozp97@gmail.com>
2021-02-26 11:43:44 +0530

    Youtube

---
+++ Youtube/app/src/main/java/com/android/youtube/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.android.youtube
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val youtube : WebView
+
+
+    }
+}
